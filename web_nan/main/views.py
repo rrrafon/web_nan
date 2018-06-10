9707a8b2a12303fe181c6a872c51e7504fb06a17
runan <rrrafon@gmail.com>
2018-06-11 00:20:41 +1200

    update commit

--- web_nan/main/views.py
+++ web_nan/main/views.py
@@ -5,15 +5,15 @@ from .models import author, authorInfo
 
 def main_page(request):
     pageTitle = 'Portfolio'
-    author_name  = author.author_name
-    author_bio = authorInfo.author_bio
+    author_name  = author.objects.get(pk = 1)
+    author_bio = authorInfo.objects.get(pk = 1)
     # should be dictionary, this is where you put variables
     context = {
         'var_title': pageTitle,
-        'var_authorName': author_name,
-        'var_authorBio': author_bio,
+        'var_authorName': author_name.author_name,
+        'var_authorBio': author_bio.author_bio,
     }
     # this code automatically look inside the templates folder
-    return render(request, 'main_index.html', context)
+    return render(request, 'main/main_index.html', context)
 
 
